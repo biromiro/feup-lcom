2c7824bcc7e23928cb97b840ff89b75bb8beab35
Goncalo Alves <up201806451@fe.up.pt>
2020-10-05 12:24:54 +0100

    Added lab2 files

---
+++ lab2/utils.c
@@ -0,0 +1,24 @@
+#include <lcom/lcf.h>
+
+#include <stdint.h>
+
+int(util_get_LSB)(uint16_t val, uint8_t *lsb) {
+  /* To be implemented by the students */
+  printf("%s is not yet implemented!\n", __func__);
+
+  return 1;
+}
+
+int(util_get_MSB)(uint16_t val, uint8_t *msb) {
+  /* To be implemented by the students */
+  printf("%s is not yet implemented!\n", __func__);
+
+  return 1;
+}
+
+int (util_sys_inb)(int port, uint8_t *value) {
+  /* To be implemented by the students */
+  printf("%s is not yet implemented!\n", __func__);
+
+  return 1;
+}
