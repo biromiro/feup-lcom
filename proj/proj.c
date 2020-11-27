e6c8e968007a7e5f457bd8ab25f87859c7899279
Nuno Costa <up201906272@fe.up.pt>
2020-11-27 17:06:49 +0000

    interrupt main loop

--- proj/proj.c
+++ proj/proj.c
@@ -32,6 +32,48 @@ int main(int argc, char *argv[]) {
   return 0;
 }
 int (proj_main_loop)(int argc, char *argv[]) {
+    int ipc_status,r,i=0;
+  uint8_t irq_set;
+  message msg;
+  uint8_t bytes[2];
 
-    return 0;
+  if(kbc_subscribe_int(&irq_set)!=0) {
+    printf("Error subscribing timer\n");
+    return 1;
+  }
+
+  vg_init(mode);
+
+  while(scancode != KBC_BRK_ESC_KEY) { /* Run until it has exceeeded time*/
+    /* Get a request message */
+    if ( (r = driver_receive(ANY, &msg, &ipc_status)) != 0 ) {
+      printf("driver_receive failed with: %d", r);
+      continue;
+    }
+    if (is_ipc_notify(ipc_status)) { /* received notification */
+      switch (_ENDPOINT_P(msg.m_source)) {
+        case HARDWARE: /* hardware interrupt notification */
+          if (msg.m_notify.interrupts &irq_set) { /* subscribed interrupt */
+            kbc_ih();
+            if(scancode == KBC_2BYTE_CODE){
+              bytes[i] = scancode;
+              i++;
+              continue;
+            }
+            bytes[i] = scancode;
+            i=0;
+          }
+          break;
+        default:
+          break; /* no other notifications expected: do nothing */
+      }
+    } else { /* received a standard message, not a notification */
+      /* no standard messages expected: do nothing */
+    }
+  }
+  
+  vg_exit();
+  kbc_unsubscribe_int();
+
+  return 0;
 }
